54f2fdf97a4213ceb7fe685b8f69440a171f4abf
Guillermo Martinelli <guillermo.martinelli@gmail.com>
2023-02-01 22:05:50 +0100

    Modularize components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { NewsComponent } from './components/news/news.component';
+import { CoinsComponent } from './components/coins/coins.component';
+import { CoinComponent } from './components/coin/coin.component';
+import { ImageComponent } from './components/image/image.component';
+import { ChartComponent } from './components/chart/chart.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavComponent, NewsComponent, CoinsComponent, CoinComponent, ImageComponent, ChartComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
